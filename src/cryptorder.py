70074448d805e8f2490392a213765c1b003a2bab
Thijs Boom <twboom@hotmail.nl>
2021-03-07 00:25:36 +0100

    Almost got it working

---
+++ src/cryptorder.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import random
+import string
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+characters = list(string.printable)
+
+def encrypt(file, offset):
+    chars = list(file)
+    encrypted = []
+    for char in chars:
+        index = characters.index(char)
+        if (index +  offset) > len(characters):
+            difference = len(characters) - index
+            remainder = len(characters) - difference
+        index = remainder
+        newChar = characters[index + offset]
+        encrypted.append(newChar)
+    return encrypted
+        
+
+path = input('Please specify file to encrypt: ')
+offset = int(input('Please set the seed: '))
+file = open(path, 'r')
+print(encrypt(file.read(), offset))
